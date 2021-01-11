70cdf7bb7b7964adb3fe1fcedf0fc39ecae0a63a
SameerMakhija <er.makhija09@gmail.com>
2021-01-11 16:50:27 +0000

    BigO - Loop through array and log times for the transactions

---
+++ DataStructure/BigO/index.js
@@ -0,0 +1,16 @@
+console.log("I am in BigO");
+
+const nemo = ["nemo"];
+const everyone = ["dory", "bruce", "marlin", "nemo", "gill"];
+const large = new Array(10000).fill("nemo");
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Forunt Nemo");
+    }
+  }
+  let t1 = performance.now();
+  console.log(`Call to find nemo took ${t1 - t0} milliseconds`);
+}
+findNemo(large);
