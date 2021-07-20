239ad4ad26470724686a812abdb74eb017f1db01
SameerMakhija <SameerMakhija@users.noreply.github.com>
2021-07-20 12:45:32 +0000

    correct file name for HashMapFirstRepeat

--- DataStructure/InterviewQuestions/HashMapFirstRepeat.js
+++ DataStructure/InterviewQuestions/HashMapFirstRepeat.js
@@ -107,18 +107,18 @@ function firstRecurringCharacter3(input) {
   }
   return undefined;
 }
-`
-function firstRecurringCharacter3(input) {
-  for(let i = 0; i < input.length - 1; i++ ){
-    for (let j = i + 1; j >= 0; j--) {
-      if (input[i] === input[j] && i !== j) {
-        return input[i];
-      }
-    }
-  }
-  return undefined;
-}
-`
+
+// function firstRecurringCharacter3(input) {
+//   for(let i = 0; i < input.length - 1; i++ ){
+//     for (let j = i + 1; j >= 0; j--) {
+//       if (input[i] === input[j] && i !== j) {
+//         return input[i];
+//       }
+//     }
+//   }
+//   return undefined;
+// }
+
 
 // Time cpmplexity = O(n^2)
 // Space complexity = O(1) <- we are not creating any new space
